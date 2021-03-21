c87c99dc187f1706ca39a9d085de0cd925fd4bc8
Eiphoria <liuerbsb@gmal.com>
2021-03-21 14:02:11 +0900

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by LIU SHURUI on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
