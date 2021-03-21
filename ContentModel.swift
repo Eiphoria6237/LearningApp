0b2dffd1ee8a95756e578dc93fbee540d2992a45
Eiphoria <liuerbsb@gmal.com>
2021-03-21 14:08:36 +0900

    organised files to folders dd content model set content model as environment object

---
+++ ContentModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by LIU SHURUI on 2021/03/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+}
---
+++ LearningApp/Models/Content.swift
@@ -0,0 +1,10 @@
+//
+//  Content.swift
+//  LearningApp
+//
+//  Created by LIU SHURUI on 2021/03/21.
+//
+
+import Foundation
+
+
