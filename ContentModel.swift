79ea0247c6f9ae1c59687c855312c223b247a726
Eiphoria <liuerbsb@gmal.com>
2021-03-21 14:46:40 +0900

    add assets at data files add models parse json/ style content in ContentModel

--- ContentModel.swift
+++ ContentModel.swift
@@ -9,5 +9,50 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
     
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // get a url to the json
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        
+        
+        // read the file into a data object
+        
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            // try to decode
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+        }
+        catch {
+            print("Couldn't parse loca data")
+        }
+        
+        // parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            // read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            //
+            self.styleData = styleData
+        }
+        catch {
+            // Log error
+            print("Couldn't parse loca data")
+        }
+    }
 }
